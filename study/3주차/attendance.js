c9f5f36929993e7e0b571f0394a02d74a4cb71c7
kangkang1106 <panda3762@naver.com>
2022-10-11 08:57:37 +0900

    20221007

--- study/3주차/attendance.js
+++ study/3주차/attendance.js
@@ -1,3 +1,8 @@
+let attendanceBtn = document.querySelector('.attendanceBtn');
+attendanceBtn.addEventListener('click', function () {
+    
+})
+
 let checkTd = document.querySelector('td');
 checkTd.addEventListener('click', function () {
     checkTd.classList.toggle('checkTd');
